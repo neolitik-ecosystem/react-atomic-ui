1113855467d965e353483fe72b9c61b7072c6671
David Ouanounou <david.ouanounou@piksel.com>
2018-01-31 09:03:37 +0100

    Create button component

--- .storybook/config.js
+++ .storybook/config.js
@@ -25,7 +25,7 @@ setOptions({
   sidebarAnimations: true
 });
 
-const req = require.context("../src/components", true, /\.stories\.js$/);
+const req = require.context("../lib", true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
