b0722f88845dc269f32366c3dd34ee7dfce40548
David Ouanounou <david.ouanounou@piksel.com>
2018-02-09 16:09:02 +0100

    Tweak Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,25 +4,87 @@ import { setOptions } from "@storybook/addon-options";
 
 // addon-info
 setDefaults({
-  header: false, // Toggles display of header with component name and description
+  header: true, // Toggles display of header with component name and description
   inline: true, // Displays info inline vs click button to view
-  source: true, // Displays the source of story Component
+  source: false, // Displays the source of story Component
   propTables: [
     /* Components used in story */
   ], // displays Prop Tables with this components
   propTablesExclude: [], // Exclude Components from being shown in Prop Tables section
-  styles: {}, // Overrides styles of addon
+  styles: {
+    infoBody: {
+      border: 'none',
+      boxShadow: 'none'
+    },
+    infoStory: {
+      padding: '20px 40px 40px',
+      textAlign: 'center'
+    },
+    infoContent: {
+    },
+  }, // Overrides styles of addon
   maxPropsIntoLine: 1 // Max props to display per line in source code
 });
 
 // addon-options
 setOptions({
-  name: "Playground",
-  showLeftPanel: true,
-  showDownPanel: true,
+  name: "Neolitik Atomic UI",
+  url: "https://neolitik-ecosystem.github.io/react-atomic-ui",
+  goFullScreen: false,
+  /**
+   * display panel that shows a list of stories
+   * @type {Boolean}
+   */
+  showStoriesPanel: true,
+  /**
+   * display panel that shows addon configurations
+   * @type {Boolean}
+   */
+  showAddonPanel: true,
+  /**
+   * display floating search box to search through stories
+   * @type {Boolean}
+   */
   showSearchBox: false,
-  downPanelInRight: true,
-  sidebarAnimations: true
+  /**
+   * show addon panel as a vertical panel on the right
+   * @type {Boolean}
+   */
+  addonPanelInRight: true,
+  /**
+   * sorts stories
+   * @type {Boolean}
+   */
+  sortStoriesByKind: true,
+  /**
+   * regex for finding the hierarchy separator
+   * @example:
+   *   null - turn off hierarchy
+   *   /\// - split by `/`
+   *   /\./ - split by `.`
+   *   /\/|\./ - split by `/` or `.`
+   * @type {Regex}
+   */
+  hierarchySeparator: '/\/|\./',
+  /**
+   * regex for finding the hierarchy root separator
+   * @example:
+   *   null - turn off mulitple hierarchy roots
+   *   /\|/ - split by `|`
+   * @type {Regex}
+   */
+  hierarchyRootSeparator: /\|/,
+  /**
+   * sidebar tree animations
+   * @type {Boolean}
+   */
+  sidebarAnimations: true,
+  /**
+   * id to select an addon panel
+   * @type {String}
+   */
+  selectedAddonPanel: undefined,
+
 });
 
 const req = require.context("../lib", true, /\.stories\.js$/);
